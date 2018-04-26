c4ac4ff8fc1e1f9f31b81f428d53e444c7119cd5
Chomtana <Chomtana001@gmail.com>
2018-04-26 10:27:49 +0700

    First prod build

--- src/css.js
+++ src/css.js
@@ -1,6 +1,21 @@
 import evx from 'eventx-core';
 
-if (!global.evx) global.evx = evx;
+if (typeof window !== "undefined" && !window.evx) window.evx = evx;
+
+function kebab(string) {
+    return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
+
+function cloneComputedStyle(target) {
+    var res = {};
+    var cs = window.getComputedStyle(target);
+    for(var i in cs) {
+        if (!isFinite(i)) {
+            res[i] = cs[i];
+        }
+    }
+    return res;
+}
 
 evx.createEvent("inlinestylechange", function (target, callback) {
     var config = { attributes: true, attributeOldValue: true, attributeFilter: ["style"] };
@@ -81,6 +96,7 @@ evx.createEvent("inlinestylechange", function (target, callback) {
                 mutation.styleName = styleName;
                 mutation.oldStyleValue = oldStyleValue;
                 mutation.newStyleValue = newStyleValue;
+                mutation.changetype = "inline";
                 //console.log(styleName);
                 if (styleName) callback(mutation);
             }
@@ -107,9 +123,80 @@ evx.createVarEvent("inlinestylechange", function (target, args, callback) {
     }
 });
 
+evx.createEvent("mediastylechange", function (target, callback) {
+    var oldcss = cloneComputedStyle(target);
+
+    //console.log("Attach",oldcss);
+
+    function filterinline() {
+        oldcss = cloneComputedStyle(this);
+    }
+
+    evx.on(target,"inlinestylechange",filterinline);
+
+    function onfire() {
+        //setTimeout(function() {
+            //console.log("onfire");
+            var newcss = cloneComputedStyle(target);
+            for (var i in newcss) {
+                if (!isFinite(i)) {
+                    /*if (i=="width") {
+                        console.log(oldcss[i],newcss[i]);
+                    }*/
+                    if (i!="cssText" && oldcss[i] != newcss[i]) {
+                        callback({
+                            target:target,
+                            styleName:kebab(i),
+                            oldStyleValue:oldcss[i],
+                            newStyleValue:newcss[i],
+                            changetype: "media"
+                        });
+                    }
+                }
+            }
+            oldcss = newcss;
+        //},1);
+    }
+    window.addEventListener('resize', onfire);
+
+    return function() { window.removeEventListener('resize', onfire); oldcss = null; }
+});
+
+evx.createVarEvent("mediastylechange", function (target, args, callback) {
+    var _callback = function(e) {
+        if (e.styleName == args[0]) callback(e);
+    }
+    evx.on(target,"mediastylechange",_callback);
+
+    return function () {
+        //ob.unobserve(target);
+        evx.off(target,"mediastylechange",_callback);
+    }
+});
+
 evx.createEvent("stylechange", function (target, callback) {
-    evx.on(target,"inlinestylechange",callback);
-    return function() { evx.off(target,"inlinestylechange",callback); }
+    function callbackinline(e) {
+        //e.changetype = "inline";
+        callback(e);
+    }
+
+    function callbackmedia(e) {
+        e.attributeName = "style";
+        e.attributeNamespace = null;
+        e.nextSibling = null;
+        e.oldValue = e.oldStyleValue;
+        e.previousSibling = null;
+        e.removedNodes = [];
+        //e.changetype = "media";
+        callback(e);
+    }
+
+    evx.on(target,"inlinestylechange",callbackinline);
+    evx.on(target,"mediastylechange",callbackmedia);
+    return function() { 
+        evx.off(target,"inlinestylechange",callbackinline);
+        evx.off(target,"mediastylechange",callbackmedia);
+    }
 });
 
 evx.createVarEvent("stylechange", function (target, args, callback) {
@@ -123,3 +210,5 @@ evx.createVarEvent("stylechange", function (target, args, callback) {
         evx.off(target,"stylechange",_callback);
     }
 });
+
+export default evx;
